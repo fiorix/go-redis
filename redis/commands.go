d0240307a9adc74c95311f41a6fc3c0ed991d8f0
Bruno Celeste <bruno.celeste@gmail.com>
2014-12-09 20:49:41 +0100

    Added SMOVE command

--- redis/commands.go
+++ redis/commands.go
@@ -784,6 +784,16 @@ func (c *Client) SMembers(key string) ([]string, error) {
 	return iface2vstr(v), nil
 }
 
+// http://redis.io/commands/smove
+func (c *Client) SMove(source string, destination string, member string) (bool, error) {
+
+	v, err := c.execWithKey(true, "SMOVE", source, destination, member)
+	if err != nil {
+		return false, err
+	}
+	return iface2bool(v)
+}
+
 // http://redis.io/commands/srandmember
 func (c *Client) SRandMember(key string, count int) ([]string, error) {
 
